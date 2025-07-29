4da2cfcec542ab826eb71abab681022869942a43
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-29 10:01:29 +0530

    Create Execute the word with a Statement.java

---
+++ Execute the word with a Statement.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner vj=new Scanner(System.in);
+        String v=vj.next();
+        System.out.println("May I know how to learn "+v+"!!!...");
+        vj.close();
+    }
+}
