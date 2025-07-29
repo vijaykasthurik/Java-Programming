d026e057236ec0a451873f88659d4ee079ac81d3
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-29 09:28:29 +0530

    Create Value Performance 1.java

---
+++ Value Performance 1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=sc.nextDouble();
+        System.out.println(a);
+        System.out.printf("%.2f",b);
+    }
+}
