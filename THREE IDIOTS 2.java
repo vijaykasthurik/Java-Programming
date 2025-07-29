bc92101fdb1a1a5f87935839a580993505867f1d
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-29 11:08:50 +0530

    Create THREE IDIOTS 2.java

---
+++ THREE IDIOTS 2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int x1,y1,y2,x2;
+        Scanner sc=new Scanner(System.in);
+        x1=sc.nextInt();
+        y1=sc.nextInt();
+        x2=sc.nextInt();
+        y2=sc.nextInt();
+        
+        System.out.println("Binoy's house is located at ("+((x1+x2)/2.0)+","+((y1+y2)/2.0)+")");
+        
+    }
+}
