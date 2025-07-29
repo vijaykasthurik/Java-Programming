968df9c0c541b76bf8fb00505a1c97b3fefed08d
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-29 10:31:11 +0530

    Create Float Formation 1.java

---
+++ Float Formation 1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner n=new Scanner(System.in);
+        float a=n.nextFloat();
+        System.out.printf("%.6f\n",a);
+        System.out.printf("%.4f\n",a);
+        System.out.println(a);
+        System.out.println(Math.round(a));
+    }
+}
