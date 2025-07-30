e1e999eedac99267932fb258b5bfdb81a5fef91a
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-30 12:29:40 +0530

    Update Check if given number is palindrome.java

--- Check if given number is palindrome.java
+++ Check if given number is palindrome.java
@@ -6,24 +6,17 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        int rev=0;
-        int cout=0;
-        int revnn=n;
-        while (n>0){
-             rev=rev*10;
-            rev+=n%10;
-            n=n/10;
-            cout+=1;
-        }
-       int revn=rev;
-        if(cout==3){
-           if(revnn==revn){
-               System.out.println("palindrome");
-           }else{
-               System.out.println("not palindrome");
-           }
-        }else{
-                System.out.println("Invalid Input");
+        if(n>=100 && n<=999){
+         int f=n/100;
+        int m=(n/10) % 10;
+        int l=n % 10;
+            if (f==l) {
+                System.out.println("palindrome");
+            } else {
+                System.out.println("not palindrome");
             }
+        } else {
+            System.out.println("Invalid Input");
+        }
     }
 }
