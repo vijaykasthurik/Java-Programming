e607c7861522cd11b3efe97c00b9dd090cefd25e
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-31 12:19:25 +0530

    Create Alphabet Right Triangle.java

---
+++ Alphabet Right Triangle.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            char ch = (char)('A' + i);
+            for (int j = 0; j <= i; j++) {
+                System.out.print((char)(ch + j) + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
