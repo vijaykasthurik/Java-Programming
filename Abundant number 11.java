2d4f531846187294d9b5a890d1bca073d103ce2c
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-31 21:58:38 +0530

    Create Abundant number 11.java

---
+++ Abundant number 11.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        for (int i=1;i<=n/2;i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        if(sum>n){
+            System.out.println("Abundant Number");
+        }else{
+            System.out.println("Not Abundant Number");
+        }
+       
+    }
+}
