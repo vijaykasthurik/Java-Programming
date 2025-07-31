58302d002c085cb4e675b9330b5c8fe7ffa78f60
VIJAY KASTHURI K <165466063+vijaykasthurik@users.noreply.github.com>
2025-07-31 11:16:20 +0530

    Create sum of even number 1.java

---
+++ sum of even number 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner (System.in);
+       int n=sc.nextInt();
+        int m=sc.nextInt();
+        int sum=0;
+        if(m>n && n>0 && m>0 && m<30 && n<30){
+            for(int i=n;i<=m;i++){
+                if(i%2==0){
+                    sum+=i;
+                }
+            }
+            System.out.println(sum);
+        }else{
+            System.out.println("Invalid Input");
+        }
+    }
+}
